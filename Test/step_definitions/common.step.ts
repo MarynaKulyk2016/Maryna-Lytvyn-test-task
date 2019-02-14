bc00f4f26a999e3b4f38b619bba889f22827ed42
marinakulik <maryna.kulyk2015@gmail.com>
2019-02-14 19:41:01 +0200

    improvements

--- Test/step_definitions/common.step.ts
+++ Test/step_definitions/common.step.ts
@@ -5,19 +5,3 @@ const { Given } = require('cucumber');
 Given(/^I am on homepage$/, async () => {
     await browser.get(browser.baseUrl);
 });
-
-Given(/^I close alert$/, async () => {
-    await browser.switchTo().alert().accept();
-});
-
-Given(/^I reload page$/, async () => {
-    await browser.refresh(1);
-});
-
-Given(/^I wait (\d+) milliseconds$/, async (delay) => {
-    await browser.sleep(+delay);
-});
-
-Given(/^I should be on "(.*)" url/, async (url) => {
-    await browser.get(url);
-});
